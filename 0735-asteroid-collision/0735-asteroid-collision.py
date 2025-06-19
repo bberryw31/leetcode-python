3d5b811b4f2a2388f609f5d04b3f0044621e63c0
Jimmy Cho <bberryw31@gmail.com>
2025-06-19 03:17:02 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,24 +1,26 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        prev_asteroids = asteroids[:]
-        while True:
-            stack = []
-            for asteroid in prev_asteroids:
-                if stack == []:
+        stack = []
+        for asteroid in asteroids:
+            if stack == []:
+                stack.append(asteroid)
+            else:
+                if asteroid * stack[-1] > 0:
                     stack.append(asteroid)
                 else:
-                    if asteroid * stack[-1] > 0:
+                    if asteroid > 0:
                         stack.append(asteroid)
                     else:
-                        if asteroid > stack[-1]:
-                            stack.append(asteroid)
-                            continue
-                        if abs(asteroid) > abs(stack[-1]):
-                            stack.pop()
-                            stack.append(asteroid)
-                        elif abs(asteroid) == abs(stack[-1]):
-                            stack.pop()
-            if stack == prev_asteroids:
-                break
-            prev_asteroids = stack[:]
+                        while stack:
+                            if asteroid * stack[-1] < 0:
+                                if abs(asteroid) > abs(stack[-1]):
+                                    stack.pop()
+                                elif abs(asteroid) == abs(stack[-1]):
+                                    stack.pop()
+                                    break
+                                else:
+                                    break
+                            else:
+                                stack.append(asteroid)
+                                break
         return stack
