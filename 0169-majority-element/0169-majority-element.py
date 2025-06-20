86a1779b0cf6bc6b15191e506f54aea0fe8f94fe
Jimmy Cho <bberryw31@gmail.com>
2025-06-20 11:26:54 -0700

    Time: 19 ms (6.15%), Space: 19.2 MB (87.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        # Initiate a hashmap to store the counts of nums elements
         num_count = {}
+        # Iterate through nums once
         for num in nums:
+            # Count the number
             num_count[num] = num_count.get(num, 0) + 1
+            # If number count is greater than half the size of nums, return the number
             if num_count[num] > len(nums) / 2:
                 return num
