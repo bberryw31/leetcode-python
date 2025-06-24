0ac06d107b3dad45f78d01825c8606f53dae31c9
Jimmy Cho <bberryw31@gmail.com>
2025-06-23 19:37:31 -0700

    Time: 0 ms (100%), Space: 18.6 MB (38.31%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,36 +5,26 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        def reverseNode(new_head):
-            current = new_head
-            previous = None
-            while current:
-                temp = current.next
-                current.next = previous
-                previous = current
-                current = temp
-            return previous
-            
-        def newHead(new_head, k):
-            start = new_head
-            current = new_head
-            previous = None
-            count = 0
-            while count < k:
-                if not current:
-                    count = -1
-                    break
-                temp = current.next
-                current.next = previous
-                previous = current
-                current = temp
-                count += 1
-            if count == -1:
-                return reverseNode(previous)
-            else:
-                start.next = newHead(current, k)
-            return previous
-
         if k == 1:
             return head
-        return newHead(head, k)
+        start = head
+        current = head
+        previous = None
+        count = 0
+        while count < k:
+            if not current:
+                current = previous
+                previous = None
+                while current:
+                    temp = current.next
+                    current.next = previous
+                    previous = current
+                    current = temp
+                return previous
+            temp = current.next
+            current.next = previous
+            previous = current
+            current = temp
+            count += 1
+        start.next = self.reverseKGroup(current, k)
+        return previous
