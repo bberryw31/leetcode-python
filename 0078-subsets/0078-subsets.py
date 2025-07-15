df6db94c01603b14b9392879641f5521172ddc95
Jimmy Cho <bberryw31@gmail.com>
2025-07-15 11:57:31 -0700

    Time: 0 ms (100%), Space: 17.9 MB (65.06%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
-        temp = []
         def helper(elements, temp):
             result.append(temp[:])
             for i in range(len(elements)):
                 temp.append(elements[i])
                 helper(elements[i+1:], temp)
                 temp.pop()
-        helper(nums, temp)
+        helper(nums, [])
         return result
