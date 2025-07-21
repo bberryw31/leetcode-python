febf6e04a5612c3abbfbc8ae72980f91ac8c9951
Jimmy Cho <bberryw31@gmail.com>
2025-07-20 20:28:26 -0700

    Time: 7 ms (83.12%), Space: 17.8 MB (87.62%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,20 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        result = []
-        candidates.sort()
-        def helper(new_candidates, temp, temp_sum):
-            if len(temp) > 150:
-                return
-            if temp_sum == target:
-                result.append(temp[:])
-                return
-            for i in range(len(new_candidates)):
-                if new_candidates[i] > target - temp_sum:
-                    return
-                temp.append(new_candidates[i])
-                temp_sum += new_candidates[i]
-                helper(new_candidates[i:], temp, temp_sum)
-                temp_sum -= temp.pop()
-        helper(candidates, [], 0)
-        return result
-                
+      candidates.sort()
+      result = []
+      
+      def helper(new_candidates, temp, temp_sum):
+        if temp_sum == target:
+          result.append(temp[:])
+          return
+
+        for i in range(len(new_candidates)):
+          if new_candidates[i] > target - temp_sum:
+            break
+          temp.append(new_candidates[i]) #2 3
+          temp_sum += new_candidates[i] #5
+          helper(new_candidates[i:], temp, temp_sum)
+          temp_sum -= temp.pop()
+
+      helper(candidates, [], 0)
+      return result
