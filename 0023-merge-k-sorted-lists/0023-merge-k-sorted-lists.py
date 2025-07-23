a2c5620a32167341da7489d9d73324f72ead5478
Jimmy Cho <bberryw31@gmail.com>
2025-07-23 15:12:56 -0700

    Time: 10 ms (65.5%), Space: 20.4 MB (17.96%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,35 +3,16 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:    
-    def merge(self,list1,list2):
-        result = ListNode()
-        it = result
-
-        while list1 and list2:
-            if list1.val < list2.val:
-                it.next = list1
-                list1 = list1.next
-            else:
-                it.next = list2
-                list2 = list2.next
-            it = it.next
-            
-        if list1:
-            it.next = list1
-        if list2:
-            it.next = list2
-        return result.next   
-
+class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        k = len(lists)
-        if k == 0:
-            return None
-
-        if k == 1:
-            return lists[0]
-
-        pre = self.mergeKLists(lists[:k//2])
-        post = self.mergeKLists(lists[k//2:])
-
-        return self.merge(pre,post)
+        heap = []
+        for node in lists:
+            while node:
+                heapq.heappush(heap, node.val)
+                node = node.next
+        res = head = ListNode()
+        while heap:
+            head.next = ListNode(heapq.heappop(heap))
+            head = head.next
+        return res.next
+                
