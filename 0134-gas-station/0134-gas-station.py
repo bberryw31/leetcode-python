84deb00459cf1faaa87caa81211a2eb7e2a1b6d4
Jimmy Cho <bberryw31@gmail.com>
2025-07-29 14:20:00 -0700

    Time: 115 ms (5.1%), Space: 25.5 MB (5.09%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,21 +1,20 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(cost) > sum(gas):
-            return -1
+        offsets = [gas_val - cost_val for gas_val, cost_val in zip(gas, cost)]
+        offsets_deque = deque(offsets)
+        tank, tank_val = [], 0
         start = 0
-        while start < len(gas):
-            if gas[start] < cost[start]:
-                start += 1
+        while offsets_deque and start < len(gas) * 2:
+            tank.append(offsets_deque.popleft())
+            start += 1
+            if tank_val + tank[-1] < 0:
+                offsets_deque.extend(tank)
+                tank, tank_val = [], 0
                 continue
-            tank = gas[start]
-            for i in range(len(cost)):
-                tank -= cost[(start + i) % len(cost)]
-                if tank < 0:
-                    start += i
-                    break
-                tank += gas[(start + i + 1) % len(cost)]
-            else:
-                return start
+            tank_val += tank[-1]
         else:
-            return -1
-            
+            if len(tank) == len(gas):
+                return start - len(gas)
+            else:
+                return -1
+                
