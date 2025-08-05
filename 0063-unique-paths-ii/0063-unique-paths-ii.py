384f2039ee805aa4291785bb5f56ad86070d4fbc
Jimmy Cho <bberryw31@gmail.com>
2025-08-05 14:07:52 -0700

    Time: 3 ms (23.31%), Space: 17.8 MB (70.76%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -4,7 +4,7 @@ class Solution:
         prev_row = [0] * (m - 1) + [1]
         for i in range(n - 1, -1, -1):
             curr_row = [0] * m
-            curr_row[-1] = prev_row[-1] if not obstacleGrid[i][-1]
+            curr_row[-1] = prev_row[-1] if not obstacleGrid[i][-1] else 0
             for j in range(m - 2, -1, -1):
                 curr_row[j] = 0 if obstacleGrid[i][j] else curr_row[j + 1] + prev_row[j]
             prev_row = curr_row[:]
